e7593400596f9f7e930e569a4b25765248b1eea8
Craigm84 <craigmorris2907@gmail.com>
2024-01-12 12:07:46 +0000

    1st exercise

---
+++ src/exercise/Exercise.java
@@ -0,0 +1,42 @@
+package exercise;
+
+public class Exercise {
+	public static void main(String[] args) {
+		
+		byte byt = 36;
+		System.out.println(byt);		
+		short sht = 290;
+		System.out.println(sht);	
+		int in = 1_200_000_000;
+		System.out.println(in);		
+		long lng = 1_234_567_890_000L;
+		System.out.println(lng);		
+		
+		char c = 'a';
+		System.out.println(c);		
+		String str = "Hello";
+		System.out.println(str);		
+		
+		float flt = 23.58F;
+		System.out.println(flt);		
+		double dbl = 23.58;
+		System.out.println(dbl);		
+		
+		boolean bln = true;
+		System.out.println(bln);
+		
+		String two = "Hello"+"Craig";
+		System.out.println(two);
+		
+//		byte byt2 = null;
+//		System.out.println(byt2); - returns an error
+		
+		char add2 = 'i' + 'i';
+		System.out.println(add2);
+		
+//		int dec = 23.58F;
+//		System.out.println(dec); - returns an error
+		
+	}
+
+}
