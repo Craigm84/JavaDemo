89befafe350aee382f3e5e45d385d67ac781a68d
Craigm84 <craigmorris2907@gmail.com>
2024-01-15 14:51:39 +0000

    Cat demo updated

---
+++ src/oop/App.java
@@ -0,0 +1,40 @@
+package oop;
+
+public class App {
+	
+	public static void main(String[] args) {
+		Cat moggy = new Cat();
+		
+		moggy.name = "Tiddles";
+		moggy.Breed = "Persian";
+		moggy.age = 12;
+		moggy.color = "Blonde";
+		
+		System.out.println(moggy.name);
+		System.out.println(moggy.Breed);
+		System.out.println(moggy.age);
+		System.out.println(moggy.color);
+	
+		Cat moggy2 = new Cat ();
+		
+		moggy2.name = "Jess";
+		moggy2.Breed = "Post Cat";
+		moggy2.age = 60;
+		moggy2.color = "Black and White";
+		
+		System.out.println(moggy2.name);
+		System.out.println(moggy2.Breed);
+		System.out.println(moggy2.age);
+		System.out.println(moggy2.color);
+		
+		moggy2.sleep();
+		moggy2.print();
+		
+		Cat[] cats = {moggy, moggy2};
+		for (Cat cat : cats) {
+			cat.print();
+		}
+		
+	}
+	
+}
---
+++ src/oop/Cat.java
@@ -0,0 +1,21 @@
+package oop;
+
+public class Cat {
+	
+	String name;
+	String Breed;
+	int age;
+	String color;
+	
+	void sleep() {
+		System.out.println("Zzzz");
+	}
+
+	void print() {
+		System.out.println(name);
+		System.out.println(Breed);
+		System.out.println(age);
+		System.out.println(color);
+	}
+
+}
