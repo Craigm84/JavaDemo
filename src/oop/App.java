a16ea5d93110a603792053a99d9b39170867df03
Craigm84 <craigmorris2907@gmail.com>
2024-01-16 11:16:51 +0000

    Encapsulation

--- src/oop/App.java
+++ src/oop/App.java
@@ -1,42 +1,32 @@
 package oop;
 
 public class App {
-	
+
 	public static void main(String[] args) {
 		Cat moggy = new Cat();
-		
-		moggy.name = "Tiddles";
-		moggy.Breed = "Persian";
-		moggy.age = 12;
-		moggy.color = "Blonde";
-		
-		System.out.println(moggy.name);
-		System.out.println(moggy.Breed);
-		System.out.println(moggy.age);
-		System.out.println(moggy.color);
-	
-		Cat moggy2 = new Cat ();
-		
-		moggy2.name = "Jess";
-		moggy2.Breed = "Post Cat";
-		moggy2.age = 60;
-		moggy2.color = "Black and White";
-		
-		System.out.println(moggy2.name);
-		System.out.println(moggy2.Breed);
-		System.out.println(moggy2.age);
-		System.out.println(moggy2.color);
-		
-		moggy2.sleep();
-		moggy2.print();
-		
+
+		moggy.setName("Tiddles");
+		moggy.setBreed("Persian");
+		moggy.setAge(12);
+		moggy.setColour("Blonde");
+
+		Cat moggy2 = new Cat();
+
+		moggy2.setName("Jess");
+		moggy2.setBreed("Post Cat");
+		moggy2.setAge(20);
+		moggy2.setColour("Black and White");
+
+//		moggy2.sleep();
+//		moggy2.print();
+
 //		Array of Cats
-		
-		Cat[] cats = {moggy, moggy2};
-		for (Cat cat : cats) { //(for each cat of cats)
+
+		Cat[] cats = { moggy, moggy2 };
+		for (Cat cat : cats) { // (for each cat of cats)
 			cat.print();
 		}
-		
+
 	}
-	
+
 }
--- src/oop/Cat.java
+++ src/oop/Cat.java
@@ -1,21 +1,56 @@
 package oop;
 
 public class Cat {
-	
-	String name;
-	String Breed;
-	int age;
-	String color;
-	
+
+	private String name;
+	private String breed;
+	private int age;
+	private String colour;
+
 	void sleep() {
 		System.out.println("Zzzz");
 	}
 
 	void print() {
 		System.out.println(name);
-		System.out.println(Breed);
+		System.out.println(breed);
 		System.out.println(age);
-		System.out.println(color);
+		System.out.println(colour);
 	}
 
+	public void setAge(int age) {
+		if (age >= 0 && age < 30) {
+			this.age = age;
+		} else {
+			System.out.println("Please provide an age between 0 and thirty");
+		}
+	}
+
+	public int getAge() {
+		return this.age;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setBreed(String breed) {
+		this.breed = breed;
+	}
+
+	public String getBreed() {
+		return this.breed;
+	}
+
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+
+	public String getColour() {
+		return this.colour;
+	}
 }
